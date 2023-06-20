80224a4cef45cf20d1ac18a165bc6c2abeab259e
ussef11 <khiraouiyoussef@gmail.com>
2023-06-20 17:23:31 +0100

    update By Ussef

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Home/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Apexchart from './apexchart';
+import DisplayChart from './Chart/DisplayChart';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
 
     <Route path="/" element={<Home />}></Route>
     <Route path="/test" element={<Apexchart />}></Route>
+    <Route path="/Chart" element={<DisplayChart />}></Route>
   </Routes>
 </BrowserRouter>
     </div>
